390ca97bb0f48b068fdce2d2a0792967c503a608
praveengothandam <mgpraveen1999@gmail.com>
2022-06-14 22:32:39 -0700

    praveen's code

--- src/org/phone/ExternalStorage.java
+++ src/org/phone/ExternalStorage.java
@@ -3,7 +3,9 @@ package org.phone;
 public class ExternalStorage {
 public void size() {
 	System.out.println("size is 128GB ");
-	
+	System.out.println("size is 128GB ");
+	System.out.println("size is 128GB ");
+	System.out.println("size is 128GB ");
 
 }
 }
